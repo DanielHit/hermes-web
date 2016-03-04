6197605db3757dc21bb2c4fc2452d995f8b928e2
hulei02 <hulei02@meituan.com>
2016-03-04 20:40:17 +0800

    change hm

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: '闪电聘'});
 });
 
 router.get('/index', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: '闪电聘'});
 });
 
 router.get('/login', function (req, res, next) {
