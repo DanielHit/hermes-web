178fd90540cc2e178674a9b98456bdf3b9afa845
liuzhengyang <liuzhengyang@meituan.com>
2016-03-05 20:34:09 +0800

    个人投递记录管理

---
+++ public/js/users/application.js
@@ -0,0 +1,37 @@
+$(function () {
+    var $table = $('#table');
+    $table.bootstrapTable({
+        url: 'deliveryJson',
+        columns: [
+            {
+                field: 'modified',
+                title: '修改日期',
+                sortable: true
+            }, {
+                field: 'job',
+                title: '职位',
+                sortable: true
+            }, {
+                field: 'recruiter',
+                title: '招聘人员',
+                sortable: true
+            }, {
+                field: 'company',
+                title: '公司',
+                sortable: true
+            }, {
+                field: 'progress',
+                title: '进度',
+                sortable: true
+            }, {
+                field: 'status',
+                title: '状态',
+                sortable: true
+            }, {
+                field: 'postTime',
+                title: '已投递时间',
+                sortable: true
+            },
+        ]
+    })
+})
