586793e7ce649a8cb37de2ea02d433af9fb95ac4
hulei02 <hulei02@meituan.com>
2016-03-12 12:37:59 +0800

    add new bootstrap

--- public/js/resume.js
+++ public/js/resume.js
@@ -9,12 +9,11 @@ function bindEvent() {
     //$("#datetimepicker").datepicker();
 }
 
-
 $(function () {
     $('#datetimepicker4').datetimepicker({
         format: 'YYYY/MM/DD',
         minDate: {
-            Default:true
+            Default: true
         }
     });
 });
