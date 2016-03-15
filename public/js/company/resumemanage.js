ea67de432a780b223fe6fad2308e1f2e63e81b76
hulei02 <hulei02@meituan.com>
2016-03-15 18:30:50 +0800

    add bootstrap table

--- public/js/company/resumemanage.js
+++ public/js/company/resumemanage.js
@@ -30,13 +30,42 @@ $(function () {
                 field: 'jobName',
                 title: '岗位名称',
                 sortable: true
-            }
-            , {
-                field: 'operation',
+            }, {
+                field: 'operate',
                 title: '操作',
-                sortable: false,
-                titleTooltip: "查看 编辑 下架"
+                align: 'center',
+                events: operateEvents,
+                formatter: operateFormatter
+            }, {
+                field: 'operate',
+                title: '选中',
+                checkbox: true,
+                align: 'center',
+                valign: 'middle'
             }
         ]
     })
 });
+
+function operateFormatter(value, row, index) {
+    return [
+        '<a class="like" href="javascript:void(0)" title="Like">',
+        '<i class="glyphicon glyphicon-heart"></i>',
+        '</a>  ',
+        '<a class="remove" href="javascript:void(0)" title="Remove">',
+        '<i class="glyphicon glyphicon-remove"></i>',
+        '</a>'
+    ].join('');
+}
+
+window.operateEvents = {
+    'click .like': function (e, value, row, index) {
+        alert('You click like action, row: ' + JSON.stringify(row));
+    },
+    'click .remove': function (e, value, row, index) {
+        $table.bootstrapTable('remove', {
+            field: 'id',
+            values: [row.id]
+        });
+    }
+};
